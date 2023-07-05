8128927d1e934b7511e923b675d2027b959c74ec
long <longlop68@gmail.com>
2023-07-06 01:57:00 +0700

    d

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
+        autoplay: false,
         smartSpeed: 1000,
         margin: 25,
         loop: true,
