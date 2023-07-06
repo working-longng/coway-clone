d32d16b4edbc26f0de4f50d33225dd81c1f06176
thanthanhduy <thanthanhduy.work@gmail.com>
2023-07-06 23:17:29 +0700

    duy

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@
             }
         } else {
             if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-white shadow').css('top', -45);
+                $('.fixed-top').addClass('bg-white shadow').css('top', 0);
             } else {
                 $('.fixed-top').removeClass('bg-white shadow').css('top', 0);
             }
